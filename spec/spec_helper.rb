68bbf9820d066eae9cf599da29e9fd6f9f5adeeb
Ahmad K <ahmed.k@hungerstation.com>
2020-10-12 15:06:07 +0300

    set coverage to 100%

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,25 @@
+# simplecov
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::Console
+SimpleCov.start
+SimpleCov.minimum_coverage(100)
+
 require "bundler/setup"
 require "ns_client"
 
 RSpec.configure do |config|
+  config.after(:suite) do
+    example_group = RSpec.describe('Code coverage')
+    example = example_group.example('must be 100%'){
+      expect( SimpleCov.result.covered_percent ).to eq 100
+    }
+    example_group.run
+    passed = example.execution_result.status == :passed
+    RSpec.configuration.reporter.example_failed example unless passed
+  end if ENV['COVERAGE']
+  
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
